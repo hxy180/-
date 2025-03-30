f847a57bbcc7d69f57633017838ee70d83d0d016
xyhou <houxiaoyu0106@qq.com>
2025-03-30 21:29:48 +0800

    更新配置文件

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/CarbonWings/',  // 将 'your-repo-name' 替换为你的 GitHub 仓库名称
   server: {
     port: 3000,
     host: true
